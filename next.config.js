bfa4a628eda9cbc892b884bff43e67ba6e9f208e
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-31 23:27:57 +0800

    fix(config/next): redirect to "/" if strapiAdminUrl is not defined

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 const strapiAdminUrl = process.env.STRAPI_ADMIN;
 
-if (!strapiAdminUrl) {
-  throw new Error("You should specify STRAPI_ADMIN environment variable.");
-}
-
 exports.default = {
   reactStrictMode: true,
   async redirects() {
     return [
       {
         source: '/admin',
-        destination: strapiAdminUrl,
+        destination: strapiAdminUrl ? strapiAdminUrl : "/",
         permanent: false,
       },
     ]
