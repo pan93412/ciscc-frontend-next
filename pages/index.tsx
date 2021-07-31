d2cad22a3797dbcadad3c7f80cf1e0001b860dca
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-31 12:52:03 +0800

    feat(pages/index): UI: done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
 import BasePage from "../components/Page/BasePage";
+import BaseButton from "../components/Elements/Button/BaseButton";
+import BaseButtonGroup from "../components/Elements/Button/Group/BaseButtonGroup";
+
+const DiscordMarkdownEditor = dynamic(
+  () => import("../components/Forms/DiscordMarkdownEditor")
+);
 
 export default function Home() {
+  const [userInput, setUserInput] = useState("");
+
   return (
-    <BasePage title="首頁" full>
-      {/* Your Content Here */}
+    <BasePage title="匿名靠北！">
+      <main>
+        <DiscordMarkdownEditor
+          userInput={userInput}
+          setUserInput={setUserInput}
+        />
+      </main>
+
+      <div className="text-center mt-8">
+        <BaseButtonGroup>
+          <BaseButton solid>送出</BaseButton>
+        </BaseButtonGroup>
+      </div>
     </BasePage>
   );
 }
