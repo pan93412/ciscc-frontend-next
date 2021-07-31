6f9df482181a526a77950b02b77d87d8dce8c7e1
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-01 02:55:04 +0800

    feat(components/Navbar): use PRODUCT_NAME value

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import BaseBadge from "../Elements/Badge/BaseBadge";
 import BaseButton from "../Elements/Button/BaseButton";
 import BaseButtonGroup from "../Elements/Button/Group/BaseButtonGroup";
+import { PRODUCT_NAME } from "../../consts";
 
 export default function Navbar() {
   return (
     <section className="p-10 grid sm:grid-cols-2 grid-cols-1 pb-4 mb-4 border-b border-gray-300 items-center">
       <div className="justify-self-center sm:justify-self-start">
         <h1 className="text-2xl font-bold text-center">
-          CISCC 靠北系統
+          {PRODUCT_NAME}
           <BaseBadge className="bg-green-800 text-white">v2.0β</BaseBadge>
         </h1>
       </div>
