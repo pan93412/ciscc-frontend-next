800666391c168248acd76c5005ec12dcc05f19fe
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-31 17:39:09 +0800

    feat(config/next): /admin redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 exports.default = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://strapi.ciscc.pan93.tk/admin',
+        permanent: false,
+      },
+    ]
+  },
 };
