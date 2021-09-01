afe7cb0a8ed4224af268432b13685e82e865aadd
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-02 01:08:12 +0800

    refactor(config/next): remove STRAPI_ADMIN
    It never worked... :(

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const strapiAdminUrl = process.env.STRAPI_ADMIN;
-
 exports.default = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/admin',
-        destination: strapiAdminUrl ? strapiAdminUrl : "/",
-        permanent: false,
-      },
-    ]
-  },
 };
